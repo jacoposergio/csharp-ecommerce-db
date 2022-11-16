8efcbaca3d86101f0dcf96fefdb2cbd16cd8bc2f
jacoposergio <djago32@gmail.com>
2022-11-16 15:56:38 +0100

    aggiunta products

---
+++ EcommerceDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+public class EcommerceDbContext : DbContext
+{
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-ecommerce;Integrated Security=True;Encrypt=false");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
-﻿
+﻿Console.WriteLine("ciao mondo");
 
-using Microsoft.EntityFrameworkCore;
+EcommerceDbContext db = new EcommerceDbContext();
 
-public class EcommerceDbContext : DbContext
-{
-    public DbSet<Payment> Payments { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<Customer> Customers { get; set; }
+//Product product = new Product();
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-ecommerce;Integrated Security=True");
-    }
-}
+//product.Name = "Orologio";
+//product.Description = "Un oggetto che serve per misurare il tempo";
+//product.Price = 130.50;
+//db.Products.Add(product);
+
+Product orologio = new Product() { Name = "Orologio", Description = "Un oggetto che serve per misurare il tempo", Price = 150.50 };
+Product sveglia = new Product() { Name = "Sveglia", Description = "Un oggetto che serve per svegliare le pesone", Price = 100 };
+Product radio = new Product() { Name = "Radio", Description = "Se non hai la tv usi questa", Price = 120.50 };
+Product forno = new Product() { Name = "Forno", Description = "Ci riscaldi le cose", Price = 76 };
+Product telefono = new Product() { Name = "Telefono", Description = "Chiami la gente", Price = 80 };
+
+db.Products.Add(orologio);
+db.Products.Add(sveglia);    
+db.Products.Add(radio);
+db.Products.Add(forno);
+db.Products.Add(telefono);
+
+db.SaveChanges();
+    
